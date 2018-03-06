2460522408d06b8ff7c50e1820b76d8144252bff
Software Engineer <aridev666@gmail.com>
2018-03-06 12:29:12 +0100

    dev

---
+++ 2018-03-06.sql
@@ -0,0 +1,19 @@
+-- Shado-Pan Wallwatcher (Creature Equipment Template)
+DELETE FROM creature_equip_template WHERE entry = 64263;
+INSERT INTO creature_equip_template (entry, id, itementry1, itementry2, itementry3) VALUES
+(64263, 3, 12403, 0, 0), -- 64263
+(64263, 2, 81358, 0, 0), -- 64263
+(64263, 1, 6680, 0, 0); -- 64263
+
+-- Shado-Pan Wallwatcher (Model Info)
+DELETE FROM creature_model_info WHERE DisplayID IN(41793, 41794, 41795, 41796);
+INSERT INTO creature_model_info (modelid, bounding_radius, combat_reach, gender, modelid_other_gender) VALUES
+(41793, 0.347222, 1.5, 0, 0),
+(41794, 0.3645831, 1.575, 0, 0),
+(41795, 0.3819442, 1.65, 0, 0),
+(41796, 0.3645831, 1.575, 0, 0);
+
+-- Shado-Pan Wallwatcher (Creature Template Addon)
+DELETE FROM creature_template_addon WHERE entry=64263;
+INSERT INTO creature_template_addon (entry, path_id, mount, bytes1, bytes2, emote, auras) VALUES
+(64263, 0, 0, 0, 1, 0, '99203');
