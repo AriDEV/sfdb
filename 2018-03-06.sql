0641a7818a98a67e74e9285f591f57823e37b528
Software Engineer <aridev666@gmail.com>
2018-03-06 13:37:37 +0100

    Update 2018-03-06.sql

--- 2018-03-06.sql
+++ 2018-03-06.sql
@@ -27,3 +27,25 @@ INSERT INTO creature_equip_template (entry, id, itementry1, itementry2, itementr
 (61082, 2, 29648, 0, 0), -- 61082
 (61082, 3, 14878, 0, 0), -- 61082
 (61083, 1, 80283, 77414, 0); -- 61083
+
+-- Supplies Kodo (Npc Vendor)
+DELETE FROM npc_vendor WHERE entry=58172;
+INSERT INTO npc_vendor (entry, slot, item, maxcount, ExtendedCost, type) VALUES
+(58172, 5, 74636, 0, 0, 1), -- 74636
+(58172, 4, 81414, 0, 0, 1), -- 81414
+(58172, 3, 81406, 0, 0, 1), -- 81406
+(58172, 2, 81412, 0, 0, 1), -- 81412
+(58172, 1, 81404, 0, 0, 1); -- 81404
+
+-- Supplies Kodo (Gossip Menu)
+DELETE FROM `gossip_menu` WHERE entry=10181;
+INSERT INTO `gossip_menu` (`entry`, `text_id`) VALUES
+(10181, 14126); -- 58172
+
+-- Supplies Kodo (Gossip Menu Option)
+DELETE FROM `gossip_menu_option` WHERE menu_id=10181;
+INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, option_id, npc_option_npcflag) VALUES
+(10181, 0, 1, 'I want to browse your goods.', 3, 128);
+
+-- Supplies Kodo (Creature Template)
+UPDATE creature_template SET gossip_menu_id=10181 WHERE entry=58172;
